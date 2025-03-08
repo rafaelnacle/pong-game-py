40fda633b1bf235390089f73643fd2d4b3c79059
= <=>
2025-03-08 16:10:46 -0300

    add scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ paddle_left = Paddle((-350, 0))
 paddle_right = Paddle((350, 0))
 
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(paddle_right.go_up, "Up")
@@ -38,9 +40,11 @@ while game_is_on:
     # Detect when Right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect when Left paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
