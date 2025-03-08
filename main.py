6164f53e4d32f6b12c2b859cb5fbbc2aae958461
= <=>
2025-03-08 15:31:03 -0300

    add collision to the ball with the paddle

--- main.py
+++ main.py
@@ -27,8 +27,13 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with the paddle
+    if ball.distance(paddle_right) < 50 and ball.xcor() > 320 or ball.distance(paddle_left) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
