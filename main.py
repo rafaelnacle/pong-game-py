5d2a053758dea69f147abc920a4f139b6b007ccf
= <=>
2025-03-08 15:06:04 -0300

    add position to constructor of paddle and create left paddle controls

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle_left = Paddle()
-paddle_right = Paddle()
-
-paddle_left.goto(x=-350, y=0)
-paddle_right.goto(x=350, y=0)
-
+paddle_left = Paddle((-350, 0))
+paddle_right = Paddle((350, 0))
 
 screen.listen()
 screen.onkey(paddle_right.go_up, "Up")
 screen.onkey(paddle_right.go_down, "Down")
+
+screen.onkey(paddle_left.go_up, "w")
+screen.onkey(paddle_left.go_down, "s")
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -4,12 +4,13 @@ PADDLE_WIDTH = 5
 PADDLE_LEN = 1
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.penup()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=PADDLE_WIDTH, stretch_len=PADDLE_LEN)
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 20
