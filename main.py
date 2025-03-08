8a78e14973e8ad7f6466c95626a9c14a3143fa66
= <=>
2025-03-08 15:26:15 -0300

    add bounce effect for the ball on top and bottom

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
@@ -26,5 +27,8 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 screen.exitonclick()
